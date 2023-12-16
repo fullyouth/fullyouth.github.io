299d0299698d8560663e28bce2fbbcc2eebf7b1e
zhanghaoqi <zhanghaoqi@shopex.cn>
2023-12-16 12:50:48 +0800

    feat(+): 增加发布功能

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run build
+
+# 进入生成的文件夹
+cd blog/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io/ <local>:<REPO>
+git push -f https://github.com/fullyouth/fullyouth.github.io.git master:zhq
